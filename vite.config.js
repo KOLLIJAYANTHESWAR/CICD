abcd8bf95c9d108a7a8e2980e2ec981dd4df5db9
KOLLIJAYANTHESWAR <kollijayanth2006@gmail.com>
2025-07-19 10:45:13 +0530

    Initial commit with Vite project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: '/CICD/', // 👈 must match your repo name exactly (case-sensitive)
   plugins: [react()],
-  base: '/CICD/'
-});
+})
